b2ab9695c2f1fc8af809343b99c99a0060647dfd
Daniel Whilchy <daniel.whilchy@jci.com>
2020-03-26 12:33:13 +0000

    6. Exercsise: Add Notifications to your App
    Adding the notification to the app and deleting the unneeded Toast.
    Also the app will not send the start notification to don´t send too many notifications to the users, keeping only the useful ones.

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -118,12 +118,12 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 }
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
-                // Step 1.5 get an instance of NotificationManager and call sendNotification
-                val notificationManager = ContextCompat.getSystemService(
-                    app,
-                    NotificationManager::class.java
-                ) as NotificationManager
-                notificationManager.sendNotification(app.getString(R.string.timer_running), app)
+//                // Step 1.5 get an instance of NotificationManager and call sendNotification
+//                val notificationManager = ContextCompat.getSystemService(
+//                    app,
+//                    NotificationManager::class.java
+//                ) as NotificationManager
+//                notificationManager.sendNotification(app.getString(R.string.timer_running), app)
 
                 // TODO: Step 1.15 call cancel notification
 
