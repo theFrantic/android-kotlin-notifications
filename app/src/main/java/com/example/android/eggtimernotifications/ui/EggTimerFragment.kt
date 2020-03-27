ec34b69fad4b72509ec2badcfe654b4b5fad1868
Daniel Whilchy <daniel.whilchy@jci.com>
2020-03-27 10:04:47 +0000

    16. Exercise: Importance
    Set priority to high

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -67,7 +67,7 @@ class EggTimerFragment : Fragment() {
                 channelId,
                 channelName,
                 // Step 2.4 change importance
-                NotificationManager.IMPORTANCE_LOW
+                NotificationManager.IMPORTANCE_HIGH
             )
             // TODO: Step 2.6 disable badges for this channel
 
